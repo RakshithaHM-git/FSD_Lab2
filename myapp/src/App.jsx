3ce6e9c729a5f6a12e72b51a8ba05c5e2d250dd4
RakshithaHM-git <rakshithahm9353@gmail.com>
2026-02-09 18:31:31 +0530

    FSD_Lab2

---
+++ myapp/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+
+
+export default function App() {
+  return (
+    <div>
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+    <Route path ='/' element ={<Home/>}/>
+    <Route path ='/about' element={<About/>} />
+       
+  </Routes>
+  </BrowserRouter>
+    </div>
+  )
+}
